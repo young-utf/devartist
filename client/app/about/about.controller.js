8c46ac01c65a7562fb763e89944168c287cf63e1
youngmoon <youngmoon@yunmi.local>
2015-01-27 08:33:04 +0900

    yunmi image uploaded

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -6,8 +6,12 @@
 angular.module('pupu')
   .controller('AboutCtrl', function ($scope, $location) {
     var path = $location.$$path;
-    
+
     if (path === '/about') {
       $('#navbar-about').addClass('active');
     }
+
+
+    //$('.about-main-image img').css('width', $(window).width() / 2);
+
   });
